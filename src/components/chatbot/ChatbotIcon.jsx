2558accac708782e0c30049224309502ced051cc
ThuongDeveloper <hoangdeptraibodoiqua4321@gmail.com>
2024-01-28 01:25:54 +0700

    fruitkha-front-end

--- src/components/chatbot/ChatbotIcon.jsx
+++ src/components/chatbot/ChatbotIcon.jsx
@@ -10,13 +10,7 @@ import {
   MessageInput,
   TypingIndicator,
 } from "@chatscope/chat-ui-kit-react";
-
-const API_KEY = process.env.CHATGPT_API_KEY;
-
-// "Explain things like you would to a 10 year old learning how to code."
-// const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
-//   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
-// }
+import axios from "axios";
 
 const ChatbotIcon = () => {
   const [showChatBot, setShowChatBot] = useState(false);
@@ -44,63 +38,25 @@ const ChatbotIcon = () => {
     const newMessages = [...messages, newMessage];
 
     setMessages(newMessages);
-
-    // Initial system message to determine ChatGPT functionality
-    // How it responds, how it talks, etc.
     setIsTyping(true);
-    await processMessageToChatGPT(newMessages);
-  };
-
-  async function processMessageToChatGPT(chatMessages) {
-    // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if (messageObject.sender === "ChatGPT") {
-        role = "assistant";
-      } else {
-        role = "user";
+    let res = await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/api/chat-with-ai`,
+      {
+        message,
       }
-      return { role: role, content: messageObject.message };
-    });
+    );
 
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act.
-    const apiRequestBody = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        // systemMessage, // The system message DEFINES the logic of our chatGPT
-        ...apiMessages, // The messages from our chat with ChatGPT
-      ],
-    };
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + API_KEY,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(apiRequestBody),
-    })
-      .then((data) => {
-        return data.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMessages([
-          ...chatMessages,
-          {
-            message: data.choices[0].message.content,
-            sender: "ChatGPT",
-          },
-        ]);
-        setIsTyping(false);
-      });
-  }
+    if (res.data.errCode === 0) {
+      setMessages([
+        ...newMessages,
+        {
+          message: res.data.chatBotMessage,
+          sender: "ChatGPT",
+        },
+      ]);
+      setIsTyping(false);
+    }
+  };
 
   const ShowHideChatBot = () => {
     setShowChatBot(!showChatBot);
